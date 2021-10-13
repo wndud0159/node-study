9bc4c87e4dad83bff9e57fdf82d2332c17d3e58f
wndud0159 <wndud0159@naver.com>
2021-10-14 03:12:38 +0900

    node-study ch05 done

---
+++ ch05/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+console.log(path.join(__dirname, 'index.html'));
+
+app.get('/', (req, res) => {
+    console.log('안찍히네')
+    return res.sendFile(path.join(__dirname, 'index.html'));
+    console.log('찍히나?');
+});
+
+app.listen(3000, () => {
+    console.log('익스프레스 서버 실행');
+});
