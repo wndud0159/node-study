2821b905e05572ed4fbe2b18a1c912deab82def0
choijuyoung <choijuyoung@choijuyoungui-MacBookPro.local>
2021-12-03 16:27:58 +0900

    test start-express

--- express/start-express/start-express.js
+++ express/start-express/start-express.js
@@ -1,4 +1,5 @@
 const fortune = require('./lib/fortune')
+const handlers = require('./lib/handler')
 
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
@@ -43,24 +44,14 @@ app.use(express.static(__dirname + '/public'))
 // })
 
 // 핸들바 사용 경우
-app.get('/', (req, res) => res.render('home'))
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-    // const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
-    res.render('about', {fortune: fortune.getFortune()})
-})
+app.get('/about', handlers.about)
 
 // custom 404 page
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
+app.use(handlers.notFound)
 
 // custom 500 page
-app.use((err, req, res, next) => {
-    console.log(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.serverError)
 
 app.listen(port, () => console.log(`Express started on http://localhost:${port}`))
